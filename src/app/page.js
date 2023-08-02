9c68541b280f783b7464fd03230063f22b393eb8
Sawyer <git@sawyerh.com>
2023-08-01 18:16:31 -0700

    use client

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,14 @@ export default function Home() {
   return (
     <ul>
       <li>
-        <Link href="/react-uswds">View React USWDS page (broken)</Link>
+        <Link href="/react-uswds">
+          View server-component with React USWDS import (broken)
+        </Link>
+      </li>
+      <li>
+        <Link href="/react-uswds-use-client">
+          View page with "use client" directive and React USWDS import (works)
+        </Link>
       </li>
       <li>
         <Link href="/plain-uswds">View non-React USWDS page (works)</Link>
---
+++ src/app/react-uswds-use-client/page.js
@@ -0,0 +1,7 @@
+"use client";
+
+import { Button } from "@trussworks/react-uswds";
+
+export default function Home() {
+  return <Button type="button">React USWDS Button</Button>;
+}
